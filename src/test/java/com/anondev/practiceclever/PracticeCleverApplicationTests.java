4273327fe6dc4f27531e1c25330954d0f86ed11d
Arsen Malyshevskyi <arsenmalyshevskyi010@gmail.com>
2021-01-07 14:54:29 +0200

    Initial commit

---
+++ src/test/java/com/anondev/practiceclever/PracticeCleverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anondev.practiceclever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeCleverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
