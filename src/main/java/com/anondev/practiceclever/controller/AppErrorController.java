6e26a1ed5d659b61e4b1e358a5e097ad4396b95a
Arsen Malyshevskyi <arsenmalyshevskyi010@gmail.com>
2021-01-07 17:36:45 +0200

    Initialize a project

---
+++ src/main/java/com/anondev/practiceclever/controller/AppErrorController.java
@@ -0,0 +1,21 @@
+package com.anondev.practiceclever.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class AppErrorController implements ErrorController{
+    @GetMapping("/error")
+    String getErrorData (HttpServletRequest request, Model model){
+        Integer errorCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        model.addAttribute("errorCode",errorCode);
+        return "err";
+    }
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
