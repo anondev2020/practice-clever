ac1a7a895e07f8fdd1aab6b75be64d6e48e8e7d8
Arsen Malyshevskyi <74253704+anondev2020@users.noreply.github.com>
2021-01-09 13:29:24 +0200

    [PNUDEV-2] Setup CD with Heroku (#7)
    * [PNUDEV-2] CD with Heroku
    
    * [PNUDEV-2] Added minor code changes

--- src/main/java/com/anondev/practiceclever/controller/AppErrorController.java
+++ src/main/java/com/anondev/practiceclever/controller/AppErrorController.java
@@ -8,12 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AppErrorController implements ErrorController{
+
     @GetMapping("/error")
     String getErrorData (HttpServletRequest request, Model model){
         Integer errorCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
         model.addAttribute("errorCode",errorCode);
-        return "err";
+        return "error";
     }
+    
     @Override
     public String getErrorPath() {
         return "/error";
